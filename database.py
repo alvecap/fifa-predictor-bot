cba41e6ef883f144c7e9c6dffd31c3b04224512d
alvecap <alvecapital60@gmail.com>
2025-03-22 14:21:11 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,10 @@
-# Ajouter cette fonction au fichier database.py si elle n'existe pas déjà
-
-def get_all_teams_alphabetical():
-    """Récupère la liste de toutes les équipes, triée et organisée par lettre."""
-    teams = get_all_teams()
+def get_all_teams():
+    """Récupère la liste de toutes les équipes"""
+    matches = get_all_matches_data()
+    teams = set()
     
-    # Organiser les équipes par première lettre
-    teams_by_letter = {}
-    for team in teams:
-        first_letter = team[0].upper()
-        if first_letter not in teams_by_letter:
-            teams_by_letter[first_letter] = []
-        teams_by_letter[first_letter].append(team)
+    for match in matches:
+        teams.add(match['team_home'])
+        teams.add(match['team_away'])
     
-    # Trier les équipes dans chaque groupe
-    for letter in teams_by_letter:
-        teams_by_letter[letter].sort()
-    
-    return teams_by_letter
+    return sorted(list(teams))

c85826a7305dd3e20af08d683acc5b5820b524b7
alvecap <alvecapital60@gmail.com>
2025-03-27 13:32:32 +0100

    Delete config_supabase.py

--- config_supabase.py
+++
@@ -1,8 +0,0 @@
-# config_supabase.py
-
-# Configuration de la base de données Supabase
-SUPABASE_URL = "https://bqhflneguvclzkzltcen.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxaGZsbmVndXZjbHpremx0Y2VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MDUxNTYsImV4cCI6MjA1ODQ4MTE1Nn0.GQZgNR_mV74cTUr0cI2DxQFjrnmRX2yiDzAaTYwh5zo"
-
-# Nombre maximum de parrainages requis
-MAX_REFERRALS = 1  # Un seul parrainage par utilisateur
